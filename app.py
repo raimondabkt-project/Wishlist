bf5e8af10e6155c3c3d39c44f60500a4edcc0221
raimondabkt-project <raimondabukit20@gmail.com>
2024-10-17 22:12:32 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://raimondabukit20:Gemer$12345@cluster0.h4pla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.dbraimonda
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 app = Flask(__name__)
 
 @app.route('/')
